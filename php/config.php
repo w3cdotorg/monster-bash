22f92aadcd943245f912ee43257728d20fc12a3e
ULB\clarrive <clement.larrive@ulb.be>
2024-05-22 21:16:19 +0200

    ajout sons

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@
         'MP3_DIRECTORY' => '../sounds/',
         'JSON_FILENAME' => '../inc/json/soundboard.json',
         'SOUNDBOARD_PAGE' => '/',
-        'TIME_ZONE' => 'America/Los_Angeles'
+        'TIME_ZONE' => 'Europe/Brussels'
     );
 ?>
 
